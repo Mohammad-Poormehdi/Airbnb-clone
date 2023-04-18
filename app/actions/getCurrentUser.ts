a8cc0afc8d1793b5c546d80b6cd70c10c01354b1
Mohammad Poormehdi <mohammad.poormehdi1383@gmail.com>
2023-04-18 19:22:17 +0430

    Date object on currentUser converted  to string
    currentUser Date object converted to string and SafeUser type created as an alternative for default User type from prisma

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -11,7 +11,7 @@ export default async function getCurrentUser(){
         if (!session?.user?.email){
             return null
         }
-        const currentUser = prisma.user.findUnique({
+        const currentUser = await prisma.user.findUnique({
             where:{
                 email:session.user.email as string
             }
@@ -19,7 +19,12 @@ export default async function getCurrentUser(){
         if(!currentUser){
             return null
         }
-        return currentUser;
+        return {
+            ...currentUser,
+            createdAt:currentUser.createdAt.toISOString(),
+            updatedAt:currentUser.updatedAt.toISOString(),
+            emailVerified: currentUser.emailVerified?.toISOString() || null,
+        };
     }catch(error:any){
         return null
     }
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,16 +1,18 @@
 'use client'
 
-import { User } from "@prisma/client";
+import { SafeUser } from "@/app/types";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 
 interface NavbarProps{
-  currentUser?: User | null
+  currentUser?: SafeUser | null
 }
 
 const Navbar:React.FC<NavbarProps> = ({currentUser}) => {
+  console.log(currentUser)
+
   return (
     <div className="fixed w-full shadow-sm z-10 bg-white">
       <div className="py-4 px-3 border-b-[1px]">
